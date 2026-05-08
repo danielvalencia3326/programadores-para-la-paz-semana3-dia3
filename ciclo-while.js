6d721aa0c527421c7613f73694e73ac4a45a62bb
danielvalencia3326 <danielvalencia3326@gmail.com>
2026-05-07 21:13:00 -0500

    modificacion de las variables

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,12 +1,16 @@
 let intento = 1
+let ciclo = 1
 const maximoIntentos = 3
+
+if (maximoIntentos < 1) {console.log ("el maximo de  intentos de ser mayor a 1")}
 
 console.log("Práctica inicial con while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
 
 while (intento <= maximoIntentos) {
-  console.log("Intento de revisión número:", intento)
+  console.log("Intento de revisión número:", intento, "numero de ciclo:", ciclo);
   intento = intento + 1
+  ciclo = ciclo + 1 
 }
 
 console.log("Proceso de revisión finalizado.")
